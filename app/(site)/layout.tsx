c7901fa0a97d2016eac979fe299c5d4f93e6aeeb
Espen Sørby <espen.sorby@rikstv.no>
2024-04-25 21:07:06 +0200

    Add Pages schema and component

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import { getPages } from '@/sanity/sanity-utils';
+import '../globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,20 +11,32 @@ export const metadata: Metadata = {
   description: 'This is where I showcase my work and projects.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pages = await getPages();
+
   return (
     <html lang="en">
       <body className={`max-w-5xl mx-auto px-5 py-8 ${inter.className}`}>
-        <header className="mt-5">
+        <header className="flex items-center justify-between">
           <Link
             href="/"
             className="text-lg font-bold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
             Home
           </Link>
+          <div className="flex items-center gap-5 text-small text-gray-600">
+            {pages.map((page) => (
+              <Link
+                key={page._id}
+                href={`/${page.slug}`}
+                className="hover:underline">
+                {page.title}
+              </Link>
+            ))}
+          </div>
         </header>
         <main className="mt-12">{children}</main>
       </body>
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,4 +1,5 @@
 import { Project } from '@/types/Project';
+import { Page } from '@/types/Page';
 import { createClient, groq } from 'next-sanity';
 import clientConfig from './config/client-config';
 
@@ -27,6 +28,30 @@ export async function getProject(slug: string): Promise<Project> {
       "image": image.asset->url,
       alt,
       url,
+      content,
+    }`,
+    { slug },
+  );
+}
+
+export async function getPages(): Promise<Page[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page"] | order(title asc){
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
+    }`,
+  );
+}
+
+export async function getPage(slug: string): Promise<Page> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
       content,
     }`,
     { slug },
